4ca7e9cb5f2ed3d9ebcd1949a3f3d59c13165ffa
Vicky <ohmnomnom90@gmail.com>
2025-06-15 19:30:48 +0300

    Add Matcher.reset()

--- src/telran/regex/selector/tools/PhotoSelector.java
+++ src/telran/regex/selector/tools/PhotoSelector.java
@@ -8,31 +8,34 @@ import java.util.regex.Pattern;
 public class PhotoSelector {
     //stringBuilder - check ->append(),->split()
     //TODO * Pattern Compile() Matcher() & Matcher Reset() Find()
-    public static String[] select(String[] input, String regex){
+    public static String[] select(String[] input, String regex) {
         List<String> result = new ArrayList<>();
-        for(String str: input){
-            if(str.matches(regex)){
+        for (String str : input) {
+            if (str.matches(regex)) {
                 result.add(str);
             }
         }
         return result.toArray(new String[0]);
     }
-    public static String[] selectSB(String[] input, String regex){
+
+    public static String[] selectSB(String[] input, String regex) {
         StringBuilder sb = new StringBuilder();
-        for(String str: input){
-            if(str.matches(regex)){
+        for (String str : input) {
+            if (str.matches(regex)) {
                 sb.append(str);
                 sb.append('\n');
             }
         }
-        return Pattern.compile("\n").split(sb,0);
+        return Pattern.compile("\n").split(sb, 0);
     }
-    public static String[] selectMatcher(String[] input, String regex){
+
+    public static String[] selectMatcher(String[] input, String regex) {
         List<String> result = new ArrayList<>();
         Pattern pattern = Pattern.compile(regex);
-        for(String str: input){
-            Matcher matcher = pattern.matcher(str);
-            if(matcher.find()){
+        Matcher matcher = pattern.matcher("");
+        for (String str : input) {
+            matcher.reset(str);
+            if (matcher.find()) {
                 result.add(str);
             }
         }
