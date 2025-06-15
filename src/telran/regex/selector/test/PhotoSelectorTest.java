0ee30759aa76a45fe44c3a65ac58f1eb4eaaf8b9
Vicky <ohmnomnom90@gmail.com>
2025-06-15 19:09:36 +0300

    Add alternative methods

--- src/telran/regex/selector/test/PhotoSelectorTest.java
+++ src/telran/regex/selector/test/PhotoSelectorTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import telran.regex.selector.tools.PhotoSelector;
 
-public class PhotoSelectorTest {
+public abstract class PhotoSelectorTest {
 
-    String[] pictures = {
+    protected String[] pictures = {
             "Paris\\20140101_090000.jpg",
             "Paris\\20140201_121005.jpg",
             "Paris\\20150301_211035.jpg",
@@ -24,9 +24,11 @@ public class PhotoSelectorTest {
     };
 
 
+    protected abstract String[] select(String[] input,String regex);
+
     @Test
     void TestEuropePictures() {
-        String mask = "(Paris|London).*";
+        String regex = "(Paris|London).*";
         String[] expected = {
                 "Paris\\20140101_090000.jpg",
                 "Paris\\20140201_121005.jpg",
@@ -42,25 +44,25 @@ public class PhotoSelectorTest {
                 "London\\20151001_231035.jpg"
 
         };
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     void TestAutumnPictures() {
-        String mask = ".*\\\\\\d{4}(09|1[01]).*";
+        String regex = ".*\\\\\\d{4}(09|1[01]).*";
         String[] expected = {
                 "London\\20151001_110023.jpg",
                 "London\\20151001_121705.jpg",
                 "London\\20151001_231035.jpg",
                 "Chicago\\20151111_232000.png"};
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     void TestSpring2015Pictures() {
-        String mask = ".*20150[3-5]\\d{2}.*";
+        String regex = ".*20150[3-5]\\d{2}.*";
         String[] expected = {
                 "Paris\\20150301_211035.jpg",
                 "Paris\\20150401_110023.gif",
@@ -68,13 +70,13 @@ public class PhotoSelectorTest {
                 "Paris\\20150501_231035.gif",
                 "Chicago\\20150301_120001.png"
         };
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     void TestNightPictures() {
-        String mask = ".*\\d{8}_(1[8-9]|2[0-3]).*";
+        String regex = ".*\\d{8}_(1[8-9]|2[0-3]).*";
         String[] expected = {
                 "Paris\\20150301_211035.jpg",
                 "Paris\\20150401_181705.jpg",
@@ -83,15 +85,15 @@ public class PhotoSelectorTest {
                 "London\\20151001_231035.jpg",
                 "Chicago\\20151111_232000.png"
         };
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     void TestNightChicagoPictures() {
-        String mask = "Chicago\\\\\\d{8}_(1[8-9]|2[0-3]).*";
+        String regex = "Chicago\\\\\\d{8}_(1[8-9]|2[0-3]).*";
         String[] expected = {"Chicago\\20151111_232000.png"};
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
 
         Assertions.assertArrayEquals(expected, actual);
 
@@ -100,7 +102,7 @@ public class PhotoSelectorTest {
 
     @Test
     void TestPngJpgPictures() {
-        String mask = ".*\\.(jpg|png)";
+        String regex = ".*\\.(jpg|png)";
         String[] expected = {
                 "Paris\\20140101_090000.jpg",
                 "Paris\\20140201_121005.jpg",
@@ -114,7 +116,30 @@ public class PhotoSelectorTest {
                 "Chicago\\20150301_120001.png",
                 "Chicago\\20151111_232000.png"
         };
-        String[] actual = PhotoSelector.select(pictures, mask);
+        String[] actual = select(pictures, regex);
         Assertions.assertArrayEquals(expected, actual);
     }
 }
+
+class PhotoSelectorSimpleTest extends PhotoSelectorTest {
+    @Override
+    protected String[] select(String[] pictures, String regex) {
+        return PhotoSelector.select(pictures, regex);
+    }
+}
+
+class PhotoSelectorSBTest extends PhotoSelectorTest {
+    @Override
+    protected String[] select(String[] pictures, String regex) {
+        return PhotoSelector.selectSB(pictures, regex);
+    }
+}
+class PhotoSelectorMatcherTest extends PhotoSelectorTest {
+    @Override
+    protected String[] select(String[] pictures, String regex) {
+        return PhotoSelector.selectMatcher(pictures, regex);
+    }
+}
+
+
+
--- src/telran/regex/selector/tools/PhotoSelector.java
+++ src/telran/regex/selector/tools/PhotoSelector.java
@@ -2,14 +2,37 @@ package telran.regex.selector.tools;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhotoSelector {
     //stringBuilder - check ->append(),->split()
     //TODO * Pattern Compile() Matcher() & Matcher Reset() Find()
-    public static String[] select(String[] input, String mask){
+    public static String[] select(String[] input, String regex){
         List<String> result = new ArrayList<>();
         for(String str: input){
-            if(str.matches(mask)){
+            if(str.matches(regex)){
+                result.add(str);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+    public static String[] selectSB(String[] input, String regex){
+        StringBuilder sb = new StringBuilder();
+        for(String str: input){
+            if(str.matches(regex)){
+                sb.append(str);
+                sb.append('\n');
+            }
+        }
+        return Pattern.compile("\n").split(sb,0);
+    }
+    public static String[] selectMatcher(String[] input, String regex){
+        List<String> result = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+        for(String str: input){
+            Matcher matcher = pattern.matcher(str);
+            if(matcher.find()){
                 result.add(str);
             }
         }
